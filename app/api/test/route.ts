e9857573342ac39396449ebcf1cc4e5dcc44df1c
Alexis Aguilar <98043211+alexisintech@users.noreply.github.com>
2024-04-08 15:02:13 -0400

    core-1 base

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -1,11 +1,23 @@
+import { clerkClient } from "@clerk/nextjs";
 import { auth } from "@clerk/nextjs/server";
 
 export async function GET() {
-  const { getToken } = auth();
+  const { userId } = auth();
 
-  const template = 'test';
+  if (!userId) {
+    return Response.json({ error: 'No authenticated user found. Please sign in.' }, { status: 401 })
+  }
 
-  const token = await getToken({ template })
+  const user = await clerkClient.users.getUser(userId);
 
-  return Response.json({ token })
+  // List out the user's email addresses
+  console.log(user.emailAddresses);
+
+  // Set the user's primary email address ID to be the ID that you chose. For this example, we'll use the second in the list.
+  const primaryEmailAddressID = user.emailAddresses[0].id;
+
+  // Update the user's primary email address
+  const response = await clerkClient.users.updateUser(userId, { primaryEmailAddressID });
+
+return Response.json({ response })
 }
