b575900fa1a831e9d27e9dca6dcc2a27ced6f77f
Alexis Aguilar <98043211+alexisintech@users.noreply.github.com>
2024-09-17 15:24:04 -0400

    update deps

---
+++ app/api/example/route.ts
@@ -0,0 +1,11 @@
+import { auth } from '@clerk/nextjs/server';
+
+export async function GET() {
+  const { getToken } = auth();
+
+  const template = 'test';
+
+  const token = await getToken({ template });
+
+  return Response.json({ token });
+}
